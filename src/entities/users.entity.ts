5690d94c7bb26d0030e9b7dcac4b022d3e679271
Alexandra Miranda <alexyamiranda7@gmail.com>
2023-03-01 14:58:33 -0300

    feat/ entidade da classe de users, schema de user e sua migração com o banco de dados

---
+++ src/entities/users.entity.ts
@@ -0,0 +1,37 @@
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+  DeleteDateColumn,
+} from "typeorm";
+
+@Entity("users")
+class User {
+  @PrimaryGeneratedColumn("increment")
+  id: number;
+
+  @Column({ type: "varchar", length: 45 })
+  name: string;
+
+  @Column({ type: "varchar", length: 45, unique: true })
+  email: string;
+
+  @Column({ type: "boolean" })
+  admin: boolean;
+
+  @Column({ type: "varchar", length: 120 })
+  password: string;
+
+  @CreateDateColumn()
+  createdAt: string;
+
+  @UpdateDateColumn()
+  updatedAt: string;
+
+  @DeleteDateColumn()
+  deletedAt: string;
+}
+
+export { User };
---
+++ src/schemas/users.schema.ts
@@ -0,0 +1,23 @@
+import { hashSync } from "bcryptjs";
+import { z } from "zod";
+
+export const userSchema = z.object({
+  name: z.string().max(45),
+  email: z.string().email().max(45),
+  admin: z.boolean(),
+  password: z
+    .string()
+    .max(120)
+    .transform((password) => {
+      return hashSync(password, 10);
+    }),
+});
+
+export const returnUserSchema = userSchema
+  .extend({
+    id: z.number(),
+    createdAt: z.date(),
+    updatedAt: z.date(),
+    deletedAt: z.date().nullable(),
+  })
+  .omit({ password: true });

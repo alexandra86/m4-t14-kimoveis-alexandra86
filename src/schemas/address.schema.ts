7fb507b7d775eda7b5dcad560292876b7b0b971e
Alexandra Miranda <alexyamiranda7@gmail.com>
2023-03-07 09:55:57 -0300

    fix/lenght da chave number de addresses e chave secreta do token

--- src/schemas/address.schema.ts
+++ src/schemas/address.schema.ts
@@ -3,7 +3,7 @@ import { z } from "zod";
 export const addressSchema = z.object({
   street: z.string().max(45),
   zipCode: z.string().max(8),
-  number: z.string().max(6).optional().nullable(),
+  number: z.string().max(7).optional().nullable(),
   city: z.string().max(20),
   state: z.string().max(2),
 });
